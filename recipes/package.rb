3ac946f867b8d81bd2710b7a083bf4b6a95ada55
ogis-shimamoto <shimamoto_koi@ogis-ri.co.jp>
2015-01-22 12:21:47 +0900

    Adding package version capability

--- recipes/package.rb
+++ recipes/package.rb
@@ -38,9 +38,8 @@ elsif platform_family?('debian')
 end
 
 package node['nginx']['package_name'] do
-  options package_install_opts
+  version node['nginx']['version']
   notifies :reload, 'ohai[reload_nginx]', :immediately
-  not_if 'which nginx'
 end
 
 service 'nginx' do
